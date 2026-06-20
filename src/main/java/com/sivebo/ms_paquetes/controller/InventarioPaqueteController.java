569df2ae7e25fd02a08c91bf213719ef96379872
daemunozr <dae.munozr@duocuc.cl>
2026-06-20 15:20:57 -0400

    normalizacion de uri, excepciones y testing

--- src/main/java/com/sivebo/ms_paquetes/controller/InventarioPaqueteController.java
+++ src/main/java/com/sivebo/ms_paquetes/controller/InventarioPaqueteController.java
@@ -20,7 +20,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/inventario")
+@RequestMapping("api/v1/inventario")
 @RequiredArgsConstructor
 public class InventarioPaqueteController {
 
